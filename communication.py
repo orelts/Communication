0062dd01802aacb7c471de308b7ae37568098522
itameyer <itameyer@gmail.com>
2021-06-15 12:29:30 +0300

    added ducomentation to communication.pp

--- communication.py
+++ communication.py
@@ -1,13 +1,23 @@
-# /usr/bin/python
+"""!
+@brief communication: client for TCP_IP. will be run on robot, and allow it to send information when ground station requests.
+"""
 import tcp_ip as com
 import time
 
-# TCP_IP = '5.29.37.154'
-TCP_IP = '127.0.0.1'
-TCP_PORT = 6800
+
+
+## public IP of ground station
+TCP_IP = '79.176.137.151'
+## port that was pre-opened on ground station (via internet router)
+TCP_PORT = 5005
 
 
 if __name__ == '__main__':
+    ##
+    # @brief try to establish communication with the ground station.
+    # once established, wait for word 'info', and upon that send a message.
+    # the message will contain readings from the sensors
+
     while True:
         try:
             conn = com.Communication(TCP_IP, TCP_PORT)
@@ -15,8 +25,8 @@ if __name__ == '__main__':
             while True:
                 try:
                     conn.transmit("-") # for server status detection
-                    ret = conn.get_cmds()
-                    conn.handle_data(ret)
+                    ret = conn.get_cmds() # recieve data from ground station
+                    conn.handle_data(ret) # if data contain word 'info' transmit readings from sensors
                 except Exception as e:
                     raise e
         except Exception as e:
